6c3dfd57e8c3026748e5c0438b709d607fec9f9b
Trần Quốc Tuyền <tranquoctuyen4995@gmail.com>
2025-09-30 09:59:20 +0700

    Deploy Flutter Web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1575557171"
+    serviceWorkerVersion: "3052062257"
   }
 });
