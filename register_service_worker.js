317f78830e6b41dab88b8512d919fc23c2937e9f
Trần Quốc Tuyền <tranquoctuyen4995@gmail.com>
2025-10-05 18:43:29 +0700

    Deploy Flutter Web

--- register_service_worker.js
+++ register_service_worker.js
@@ -1,19 +1,14 @@
-console.log("File js loaded \n");
 if ("serviceWorker" in navigator) {
-  console.log("ServiceWorker is running... \n");
   const baseEl = document.querySelector("base");
   const baseHref =
     baseEl && baseEl.getAttribute("href") ? baseEl.getAttribute("href") : "/";
   const swUrl = baseHref + "flutter_service_worker.js";
-  console.log("[SW] Registering service worker at", swUrl);
 
   navigator.serviceWorker.register(swUrl).then((reg) => {
     // Lắng nghe khi có service worker mới
-    console.log("Listening is running... \n");
     reg.onupdatefound = () => {
       const newWorker = reg.installing;
       newWorker.onstatechange = () => {
-        console.log("Onstatechange is running... \n");
         if (
           newWorker.state === "installed" &&
           navigator.serviceWorker.controller
