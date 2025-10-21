20931149e056b69a5f163a854404ad274d1d811f
Trần Quốc Tuyền <tranquoctuyen4995@gmail.com>
2025-10-22 00:00:48 +0700

    Deploy Flutter Web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2026159089"
+    serviceWorkerVersion: "1119754852"
   }
-, mainDartJsUrl: "https://cdn.jsdelivr.net/gh/quoctuyenit/note-work@gh-pages/main.dart.js"});
+});
